db1a9d00c189cf070c92a2f369e273b04be33bf4
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-06-15 17:30:33 +0200

    antoine - enabled signup - login - session

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,20 +1,102 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const User = require('../models/User.model');
 
-const signup = (req, res, next) => {
+const saltRound = 10;
+
+const getSignup = (req, res, next) => {
   res.render('auth/signup');
 }
 
-const login = (req, res, next) => {
+const postSignup = (req, res, next) => {
+  const { username, email, password } = req.body
+
+  if (!username || !email || !password) {
+    res.status(500).render('auth/signup', { 
+      errorMessage: 'Please fill all the fields.'   
+    });
+    return;
+  }
+  
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).render('auth/signup', { 
+      errorMessage: 'The password needs to have at least 6 characters and must contain at least one number, one lowercase and one uppercase letter.' 
+    });
+    return;
+  }
+
+  bcrypt.genSalt(saltRound)
+  .then(salt => bcrypt.hash(password,salt))
+  .then(hashedPassword => {
+    User.create({
+      username: username,
+      email: email,
+      password: hashedPassword
+    })
+    .then(user => {
+      req.session.currentUser = user;
+      res.redirect('/profile')
+    })
+    .catch(error => {
+      // Email error
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(400).render('auth/signup', {
+            errorMessage: error.message
+        })
+      // User already exists
+      } else if (error.code === 11000) {
+        res.status(400).render('auth/signup', {
+          errorMessage: "This username already exists"
+        })
+      } else {
+        console.log(error)
+      }
+    })
+  })
+  .catch(err => next(err))
+}
+
+const getLogin = (req, res, next) => {
   res.render('auth/login');
 }
 
+const postLogin = (req, res, next) => {
+  const {email, password} = req.body;
+
+  if (!email || !password) {
+    res.status(500).render('auth/signup', { 
+      errorMessage: 'Please fill all the fields.'   
+    });
+    return;
+  }
+  
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: "Wrong email" })
+        return
+      } else if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.redirect('/')
+      } else {
+        res.render('auth/login', { errorMessage: "Wrong password" })
+        return
+      }
+    })
+    .catch (err => next(err))
+}
+
 const getProfile = (req, res, next) => {
-  res.render('users/profile');
+  const user = req.session.currentUser
+  console.log(user)
+  res.render('users/profile', {user});
 }
 
 module.exports = {
-  signup,
-  login,
+  getSignup,
+  postSignup,
+  getLogin,
+  postLogin,
   getProfile
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 
-const {signup, login, getProfile} = require('../controllers/auth.controller')
+const {getSignup, postSignup, getLogin, postLogin, getProfile} = require('../controllers/auth.controller')
 
 router
-.get('/signup', signup)
-.get('/login', login)
+.get('/signup', getSignup)
+.post('/signup', postSignup)
+.get('/login', getLogin)
+.post('login', postLogin)
 .get('/profile', getProfile)
 
 module.exports = router;
