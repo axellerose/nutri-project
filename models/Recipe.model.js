c0fa2d099ed11a0f7d821a608f2a659989dd0767
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-06-16 21:30:51 +0200

    started coding the recipe creation, to be continued (no routes yet)

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,8 +1,22 @@
 const { Schema, model } = require('mongoose');
+const Product = require('../models/Product.model')
 
 const recipeSchema = new Schema(
   {
-   etc: "etc"
+    name: {
+      type: String,
+      required: [true, 'Name is required.'],
+    },
+    image: {
+      type: String,
+      required: true,
+      // default
+    },
+    time: Number,
+    products: [{ type: Schema.Types.ObjectId, ref: 'Product'}],
+    info: {},
+    reviews: [String],
+    rank: Number
   },
   {
     timestamps: true
