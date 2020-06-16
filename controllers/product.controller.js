5f13b3547b253fc5a6f9b1d5db9de76f845bc83e
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-06-16 10:01:41 +0200

    Profile/Logout and Signup/Login appear depending on the existence of a user session

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs')
 
 const products = (req, res, next) => {
-  res.render('products/index');
+  const user = req.session.currentUser
+  res.render('products/index', {user});
 }
 
 module.exports = {
