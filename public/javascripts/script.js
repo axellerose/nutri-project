31ff3e8e91b629ade16c5ed12896439942e015e8
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-06-18 13:17:32 +0200

    tests add various products

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,43 @@
+let arrayOfProducts = []
+
+let html = ''
 document.getElementById("new-ingredient").addEventListener("click", function(){
   event.preventDefault()
-  let html = ''
+  
   const e = document.getElementById("products")
-  const name = e.options[e.selectedIndex].text
+  console.log("TYPE OF E: ", typeof(e), e)
+  // let ingredient = JSON.parse(JSON.stringify(e.options[e.selectedIndex]))
+  let ingredient = e.options[e.selectedIndex].innerHTML
+  console.log("TYPE: ",typeof(ingredient))
+  console.log(ingredient)
   const quantity = document.getElementById("quantity").value
+  const newIngredient = {
+    ingredient: ingredient,
+    quantity: quantity
+  }
+  arrayOfProducts.push(newIngredient)
   html += `
   <li>
-    ${name}     ${quantity}
+    ${ingredient.name}     ${quantity}
   </li>
   `
-  console.log(name)
-  console.log(quantity)
+  console.log("NAME :", ingredient.name)
+  console.log("QUANTITY", quantity)
   document.getElementById('ingredients-list').innerHTML = html
+  console.log("ARRAY OF PRODUCTS", arrayOfProducts)
 });
+
+document.getElementById("validar-ingredients").addEventListener("click", function(){
+  event.preventDefault()
+
+  const e = document.getElementById("products")
+  e.options[e.selectedIndex].value = arrayOfProducts
+});
+
+
+
+
+// [{productObject,quantity},{productObject,quantity}]
+
+// Convert HTML Form Fields to JSON
+// https://www.learnwithjason.dev/blog/get-form-values-as-json/
