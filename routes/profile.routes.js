f515c616879d842d2d885bc27f64f1053d51af8d
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-06-24 14:21:45 +0200

    started creation of favorites-recipes, doesnt work yet

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router  = express.Router();
 
-const {getProfile, getMyRecipes} = require('../controllers/profile.controller');
+const {getProfile, getMyRecipes, getMyFavoriteRecipes} = require('../controllers/profile.controller');
 
 router
 .get('/', getProfile)
 .get('/my-recipes', getMyRecipes)
+.get('/favorite-recipes', getMyFavoriteRecipes)
 
 module.exports = router;
