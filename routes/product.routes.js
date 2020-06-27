1c5d6504db37029a293640c223b44c54dfa57cf6
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-06-27 14:14:23 +0200

    enabled cloudinary for products

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router  = express.Router();
+const fileUploader = require('../configs/cloudinary.config');
 
 const {getProducts,getFeed,postFeed,getProductDetails,getDeleteProduct} = require('../controllers/product.controller')
 
 router
 .get('/', getProducts)
 .get('/feedDb', getFeed)
-.post('/feedDb', postFeed)
+.post('/feedDb', fileUploader.single('image'), postFeed)
 .get('/details/:name', getProductDetails)
 .get('/delete/:name', getDeleteProduct)
 
